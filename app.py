cb76531d87d5b40c0c85874511a24824a42e4a58
tylerminnis <tylerminnis10@outlook.com>
2022-11-13 15:57:38 -0500

    updated home page with cards

--- app.py
+++ app.py
@@ -8,26 +8,26 @@ app = Flask(__name__)
 def home():
     return render_template('index.html') 
 
-@app.get('/about.html')
+@app.get('/about')
 def about():
     return render_template('about.html') 
 
-@app.get('/search.html')
+@app.get('/search')
 def search():
     return render_template('search.html') 
 
-@app.get('/all_games.html')
+@app.get('/all_games')
 def all_games():
     return render_template('all_games.html') 
 
-@app.get('/profile.html')
+@app.get('/profile')
 def profile():
     return render_template('profile.html') 
 
-@app.get('/post_review.html')
+@app.get('/post_review')
 def post_review():
     return render_template('post_review.html') 
 
-@app.get('/gamepage.html')
+@app.get('/gamepage')
 def gamepage():
     return render_template('gamepage.html') 
