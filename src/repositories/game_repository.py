61a74a24934612bbf85a39afeb6ab649b1fe4196
tylerminnis <tylerminnis10@outlook.com>
2022-11-29 19:30:38 -0500

    fixed create_game

--- src/repositories/game_repository.py
+++ src/repositories/game_repository.py
@@ -2,22 +2,19 @@ from src.models.game import Game, db
 
 #credit Krevat for code inspo
 class GameRepository:
-    """Game holds title, developer, and rating"""
-    # these eventually need to be changed to SQLAlchemy queries
-    def __init__(self) -> None:
-        self._db: list[Game] = []
 
     def get_all_games(self):
         game_list = Game.query.all()
         return game_list
 
     def get_game_by_title(self, title: str) -> Game | None:
-        """Get a single game by its title or None if it does not exist"""
         game = Game.query.filter_by(title=title).first()
 
     def create_game(self, title: str, developer: str, rating: float) -> Game:
-        """Create a new game and return it"""
         create_game = Game(game_id = game_id, title=title, publisher=publisher, description=description, developer=developer, thumbnail_link=thumbnail_link, release_date=release_date)
+        db.session.add(create_game)
+        db.session.commit()
+        return create_movie
 
     def search_games(self, title):
         search_game = Game.query.filter(Game.title.ilike(f'%{title}%')).all() 
