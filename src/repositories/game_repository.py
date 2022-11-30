9b5175c2409ba6b14045fa538e438654557fb37e
tylerminnis <tylerminnis10@outlook.com>
2022-11-29 19:19:13 -0500

    updated game_repository.py to use SQLAlchemy

--- src/repositories/game_repository.py
+++ src/repositories/game_repository.py
@@ -1,34 +1,25 @@
-from src.models.game import Game
-
-_game_repo = None
+from src.models.game import Game, db
 
 #credit Krevat for code inspo
-def get_game_repository():
-    global _game_repo
+class GameRepository:
+    """Game holds title, developer, and rating"""
+    # these eventually need to be changed to SQLAlchemy queries
+    def __init__(self) -> None:
+        self._db: list[Game] = []
 
-    class GameRepository:
-        """Game holds title, developer, and rating"""
-        # these eventually need to be changed to SQLAlchemy queries
-        def __init__(self) -> None:
-            self._db: list[Game] = []
+    def get_all_games(self):
+        game_list = Game.query.all()
+        return game_list
 
-        def get_all_games(self, games):
-            return self._db
+    def get_game_by_title(self, title: str) -> Game | None:
+        """Get a single game by its title or None if it does not exist"""
+        game = Game.query.filter_by(title=title).first()
 
-        def get_game_by_title(self, title: str) -> Game | None:
-            """Get a single game by its title or None if it does not exist"""
-            for game in self._db:
-                if game.title == title:
-                    return game
-            return None
+    def create_game(self, title: str, developer: str, rating: float) -> Game:
+        """Create a new game and return it"""
+        create_game = Game(game_id = game_id, title=title, publisher=publisher, description=description, developer=developer, thumbnail_link=thumbnail_link, release_date=release_date)
 
-        def create_game(self, title: str, developer: str, rating: float) -> Game:
-            """Create a new game and return it"""
-            game = Game(title, developer, rating)
-            self._db.append(game)
-            return game
-
-    if _game_repo is None:
-        _game_repo = GameRepository()
+    def search_games(self, title):
+        search_game = Game.query.filter(Game.title.ilike(f'%{title}%')).all() 
     
-    return _game_repo
+game_repository_singleton = GameRepository()
